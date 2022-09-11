288625f013b3f277703f52bac2cbcd07e6695f82
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-09-11 08:49:10 +0800

    copied firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,24 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAKKubzjp6rlGfDMT0tw_lYbEyi-xcnmhU",
-    authDomain: "react-slack-403ea.firebaseapp.com",
-    projectId: "react-slack-403ea",
-    storageBucket: "react-slack-403ea.appspot.com",
-    messagingSenderId: "400074732951",
-    appId: "1:400074732951:web:529897fdcfa6cd2e8bc0d2"
-  };
+  apiKey: "AIzaSyDTFlISuAL1r9szlqhNZxgGwZyemiba2Io",
+  authDomain: "react-slack-c1254.firebaseapp.com",
+  projectId: "react-slack-c1254",
+  storageBucket: "react-slack-c1254.appspot.com",
+  messagingSenderId: "542679660600",
+  appId: "1:542679660600:web:9530edb9aff8d9dc5a8ecd",
+  measurementId: "G-PGS8PEMC2P",
+};
 
-  // Use this to initialize the firebase App
-  const firebaseapp = firebase.initializeApp(firebaseConfig);
+// Use this to initialize the firebase App
+const firebaseapp = firebase.initializeApp(firebaseConfig);
 
-  // Use these for db, auth & provider
-  const db = firebaseapp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+// Use these for db, auth & provider
+const db = firebaseapp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export { db, auth, provider }
+export { db, auth, provider };
